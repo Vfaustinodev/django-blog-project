6e0e386e7750637379f35ccca595cc4a78e8a894
Virginio <Virginio>
2024-08-15 17:44:34 -0300

    Sem Modificações

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,4 +1,4 @@
-from blog.views import index, post, page
+from blog.views import index, post, page, created_by, category, tag
 from django.urls import path
 
 #namespace
@@ -7,5 +7,9 @@ app_name = 'blog'
 urlpatterns = [
     path('', index, name='index'),
     path('post/<slug:slug>/', post, name='post'),
-    path('page/', page, name='page'),
+    path('page/<slug:slug>/', page, name='page'),
+    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path('category/<slug:slug>/', category, name='category'),
+    path('tag/<slug:slug>/', tag, name='tag'),
+    
 ]
--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -19,7 +19,52 @@ def index(request):
         }
     )
 
-def page(request):
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk) #type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug) #type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def tag(request, slug):
+    posts = Post.objects.get_published().filter(tags__slug=slug) #type: ignore
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def page(request, slug):
 
     return render(
         request,
@@ -29,12 +74,17 @@ def page(request):
         }
     )
 
-def post(request):
+def post(request, slug):
 
+    post = (
+        Post.objects.get_published() #type: ignore
+        .filter(slug=slug)
+        .first()
+    )
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
         }
     )
